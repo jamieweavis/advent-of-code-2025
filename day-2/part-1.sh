8a52fb1209d49a50a09612c05d59ec951d438580
Jamie Weavis <8133259+jamieweavis@users.noreply.github.com>
2025-12-03 09:17:40 +0000

    Add day 2 solutions

---
+++ day-2/part-1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function solve() {
+  while read line; do
+    IFS='-' read -ra range <<< "$line"
+    start=${range[0]}
+    end=${range[1]} 
+    for (( i=start; i<=end; i++ )); do echo $i; done
+  done
+}
+
+cat "$1" \
+  | tr ',' '\n' \
+  | solve \
+  | grep -E '^([0-9]+)\1$' \
+  | paste -s -d '+' - \
+  | bc
--- makefile
+++ makefile
@@ -7,3 +7,13 @@
 1:
 	make 1-1
 	make 1-2
+
+2-1:
+	bash day-2/part-1.sh day-2/input.txt
+
+2-2:
+	bash day-2/part-2.sh day-2/input.txt
+
+2:
+	make 2-1
+	make 2-2
